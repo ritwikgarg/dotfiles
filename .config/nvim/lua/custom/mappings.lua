040df6a81fd769b9a66817780ac11604c68760ae
Ritwik Garg <ritwik@Ritwik.Garg@merai.co>
2025-07-10 16:21:12 +0530

    Fix: add nvim as a regular folder instead of submodule

---
+++ .config/nvim/lua/custom/mappings.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+M.general = {
+  n = {
+    ["<C-h>"] = {"<cmd> TmuxNavigateLeft<CR>", "window left"},
+    ["<C-l>"] = {"<cmd> TmuxNavigateRight<CR>", "window right"},
+    ["<C-j>"] = {"<cmd> TmuxNavigateDown<CR>", "window down"},
+    ["<C-k>"] = {"<cmd> TmuxNavigateUp<CR>", "window up"},
+  }
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>" },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+    end,
+  },
+}
+return plugins
